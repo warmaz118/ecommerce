f81a7def660b5ec8e8ae2c6c84e2fd95cda80160
warmaz118 <mikhsaan118@gmail.com>
2024-09-24 19:41:09 +0700

    menambahkan resource product dan order item

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\ProductResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                    Section::make('Product Information')->schema([
+                        TextInput::make('name')
+                        ->required()
+                        ->maxLength(255)
+                        ->live(onBlur:true)
+                        ->afterStateUpdated(function (string $operation, $state, Set $set ) {
+                            if ($operation !== 'create') {
+                               return;
+                            }
+                            $set('slug', Str::slug($state)); 
+                        }), 
+                        TextInput::make('slug')
+                        ->required()
+                        ->maxLength(255)
+                        ->disabled()
+                        ->dehydrated()
+                        ->unique(Product::class, 'slug', ignoreRecord:true),
+                        
+                    MarkdownEditor::make('description')
+                        ->columnSpanFull()
+                        ->fileAttachmentsDirectory('products')
+                    ])->columns(2),
+
+                    Section::make('Images')->schema([
+                        FileUpload::make('images')
+                        ->multiple()
+                        ->directory('products')
+                        ->maxFiles(5)
+                        ->reorderable()
+                    ])
+                ])->columnSpan(2),
+
+            Group::make()->schema([
+                Section::make('Price')->schema([
+                    TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR')
+                ]),
+                Section::make('Associations')->schema([
+                    Select::make('category_id')
+                    ->required()
+                    ->searchable()
+                    ->preload()
+                    ->relationship('category', 'name'),
+                    Select::make('brand_id')
+                    ->required()
+                    ->searchable()
+                    ->preload()
+                    ->relationship('brand', 'name'),
+                ]),
+
+                Section::make('Status')->schema([
+                    Toggle::make('instock')
+                    ->required()
+                    ->default(true),
+                    Toggle::make('isactive')
+                    ->required()
+                    ->default(true),
+                    Toggle::make('isfeatured')
+                    ->required(),
+                    Toggle::make('onsale')
+                    ->required()
+                ])
+            ])->columnSpan(1)
+
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                ->searchable(),
+                TextColumn::make('category.name')
+                ->sortable(),
+                TextColumn::make('price')
+                ->money('IDR')
+                ->sortable(),
+                TextColumn::make('brand.name')
+                ->sortable(),
+                IconColumn::make('isfeatured')
+                ->boolean(),
+                IconColumn::make('onsale')
+                ->boolean(),
+                IconColumn::make('instock')
+                ->boolean(),
+                IconColumn::make('isactive')
+                ->boolean(),
+                TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable('isToggleHiddenByDefault:true'),
+                TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable('isToggleHiddenByDefault:true'),
+                ])
+            ->filters([
+                SelectFilter::make('category')
+                ->relationship('category','name'),
+                SelectFilter::make('brand')
+                ->relationship('brand','name')
+            ])
+            ->actions([
+                ActionGroup::make([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
